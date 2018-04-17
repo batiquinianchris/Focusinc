6317db40fc4b04c94d6736d3e234874960d0dd86
Darell Domalaon <darelldomalaon@gmail.com>
2018-04-17 09:46:34 +0800

    update

--- Focusinc/loginForm.Designer.cs
+++ Focusinc/loginForm.Designer.cs
@@ -28,20 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // loginForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 333);
-            this.Name = "loginForm";
-            this.Text = "Login";
-            this.ResumeLayout(false);
-
+            this.Text = "loginForm";
         }
 
         #endregion
     }
 }
-
